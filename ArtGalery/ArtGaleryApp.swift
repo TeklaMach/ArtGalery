ed7ac30b6640019cf665f9815c92ed1bc3d2d4b2
TeklaMach <macharashvilitekla@gmail.com>
2023-12-22 06:33:53 +0400

    Initial Commit

---
+++ ArtGalery/ArtGaleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArtGaleryApp.swift
+//  ArtGalery
+//
+//  Created by Tekla Matcharashvili on 22.12.23.
+//
+
+import SwiftUI
+
+@main
+struct ArtGaleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
